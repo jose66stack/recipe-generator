2bfd61ab9351af30d02c15c7988abd5535abbbe9
jose66stack <jfp127631@gmail.com>
2023-12-02 02:31:39 -0600

    two

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
@@ -8,15 +6,26 @@ const App = () => {
   const APP_ID = "b0ab424e"
   const APP_KEY = "00275adb142accc6a956584433747571"
   
-  const exampleReq = 
-  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([]);
+
+
+ useEffect( () => {
+  getRecipes();
+ }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+  
 
 
   return(
     <div className='App'>
-      <form>
-        <input type='text'/>
-        <button type="submit"></button>
+      <form className='search-form'>
+        <input className='search-bar' type='text'/>
+        <button className='search-button' type="submit">Search</button>
       </form>
     </div>
   )

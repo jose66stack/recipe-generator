88fc0b50196c9d0fb5567fe03de65012972a525c
jose66stack <jfp127631@gmail.com>
2023-12-02 10:22:59 -0600

    Recipe v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,54 @@
-import { useEffect, useState } from 'react'
-import Recipe from './Recipe'
-import './App.css'
+import { useEffect, useState } from 'react';
+import Recipe from './Recipe';
+import './App.css';
 
 const App = () => {
-  
-  const APP_ID = "b0ab424e"
-  const APP_KEY = "00275adb142accc6a956584433747571"
-  
+  const APP_ID = "b0ab424e";
+  const APP_KEY = "00275adb142accc6a956584433747571";
+
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken');
 
-
- useEffect( () => {
-  getRecipes();
- }, []);
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits)
-  }
-  
+    console.log(data.hits);
+  };
 
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
-  return(
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
+  return (
     <div className='App'>
-      <form className='search-form'>
-        <input className='search-bar' type='text'/>
+      <form className='search-form' onSubmit={getSearch}>
+        <input className='search-bar' type='text' value={search} onChange={updateSearch} />
         <button className='search-button' type="submit">Search</button>
       </form>
-      {recipes.map(recipe => (
-        <Recipe 
-        title={recipe.recipe.label} 
-        calories={recipe.recipe.calories}
-        image={recipe.recipe.image}
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
         />
       ))}
     </div>
-  )
+  );
+};
 
-}
-export default App
+export default App;
